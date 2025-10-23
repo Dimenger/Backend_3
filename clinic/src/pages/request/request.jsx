0ebadda8e8b8cc2ef2ce89789093438c2c262f3b
Dmitry Seregin <dimenerDS@gmail.com>
2025-10-23 19:39:38 +0400

    Fix

--- clinic/src/pages/request/request.jsx
+++ clinic/src/pages/request/request.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import InputMask from "react-input-mask";
+
 import styles from "./request.module.css";
 
 export const Request = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [description, setDescription] = useState("");
+  const [isSending, setIsSending] = useState(false);
 
   const handleSendRequest = async (e) => {
     e.preventDefault();
+
+    setIsSending(true);
 
     const date = new Date().toISOString().slice(0, 10);
     const time = new Date().toISOString().slice(11, 16);
@@ -32,56 +37,65 @@ export const Request = () => {
     setName("");
     setPhone("");
     setDescription("");
+    setTimeout(() => {
+      setIsSending(false);
+    }, 3000);
   };
 
   return (
-    <div className={styles.form_container}>
-      <form className={styles.form} onSubmit={handleSendRequest}>
-        <h1 className={styles.title}>Запись к врачу</h1>
-        <div className={styles.name}>
-          <label htmlFor="name" className={styles.label}>
-            ФИО
-          </label>
-          <input
-            name="name"
-            id="name"
-            type="text"
-            value={name}
-            placeholder="Фамилия_Имя_Отчество"
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className={styles.phone}>
-          <label htmlFor="phone" className={styles.label}>
-            Телефон
-          </label>
-          <input
-            name="phone"
-            id="phone"
-            type="text"
-            placeholder="+7 (___) ___-__-__"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-          />
-        </div>
-        <div className={styles.request}>
-          <label htmlFor="request" className={styles.label}>
-            Обращение
-          </label>
-          <textarea
-            name="request"
-            id="request"
-            rows="4"
-            cols="50"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <input type="submit" value="Отправить данные" />
-      </form>
-    </div>
+    <>
+      <div className={styles.form_container}>
+        <h2 className={isSending ? styles.messageInvisible : styles.message}>
+          Ваше сообщение отправлено!
+        </h2>
+
+        <form className={styles.form} onSubmit={handleSendRequest}>
+          <h1 className={styles.title}>Запись к врачу</h1>
+          <div className={styles.name}>
+            <label htmlFor="name" className={styles.label}>
+              ФИО
+            </label>
+            <input
+              name="name"
+              id="name"
+              type="text"
+              value={name}
+              placeholder="Фамилия_Имя_Отчество"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles.phone}>
+            <label htmlFor="phone" className={styles.label}>
+              Телефон
+            </label>
+            <input
+              name="phone"
+              id="phone"
+              type="tel"
+              placeholder="+7 ___ ___-__-__"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles.request}>
+            <label htmlFor="request" className={styles.label}>
+              Обращение
+            </label>
+            <textarea
+              name="request"
+              id="request"
+              rows="4"
+              cols="50"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <input type="submit" value="Отправить данные" disabled={isSending} />
+        </form>
+      </div>
+    </>
   );
 };
